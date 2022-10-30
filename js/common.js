dc59b83463187c7db4f467da73050370615d2b0e
SubinNoh <snoh6839@mysvc.skagit.edu>
2022-10-30 21:58:09 +0900

    ....

--- js/common.js
+++ js/common.js
@@ -1,7 +1,21 @@
 const moreBtn = document.querySelector('.info .metadata .moreBtn');
 const title = document.querySelector('.info .metadata .title');
+const upDown = $(".actions li button i");
 
 moreBtn.addEventListener('click', () => {
   moreBtn.classList.toggle('clicked');
   title.classList.toggle('clamp');
 })
+
+
+upDown.on("click",function () {
+  if (upDown.hasClass("active")) {
+    upDown.removeClass("active")
+  }else {
+    upDown.addClass("active")
+  }
+})
+
+// upDown.each(function functionName() {
+//
+// })
