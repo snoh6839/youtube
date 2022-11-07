5ab5ae863507abd22f0976a70f0485535d130b53
SubinNoh <snoh6839@mysvc.skagit.edu>
2022-11-07 09:34:32 +0900

    qjxms

--- js/common.js
+++ js/common.js
@@ -1,23 +1,27 @@
 const moreBtn = document.querySelector('.info .metadata .moreBtn');
 const title = document.querySelector('.info .metadata .title');
-const upDown = $(".actions li button i");
+const thumUp = document.querySelector(".actions li button .fa-thumbs-up");
+const thumDown = document.querySelector(".actions li button .fa-thumbs-down");
 
 moreBtn.addEventListener('click', () => {
   moreBtn.classList.toggle('clicked');
   title.classList.toggle('clamp');
 })
 
-// upDown.addEventListener('click', () => {
-//   upDown.classList.toggle('active');
-// })
-
-upDown.on("click",function () {
-  if (upDown.hasClass("active")) {
-    upDown.removeClass("active")
-  }else {
-    upDown.addClass("active")
-  }
+thumUp.addEventListener('click', () => {
+  thumUp.classList.toggle('active');
 })
+thumDown.addEventListener('click', () => {
+  thumDown.classList.toggle('active');
+})
+
+// upDown.on("click",function () {
+//   if (upDown.hasClass("active")) {
+//     upDown.removeClass("active")
+//   }else {
+//     upDown.addClass("active")
+//   }
+// })
 
 // upDown.each(function functionName() {
 //
